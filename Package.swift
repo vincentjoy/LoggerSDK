5f19bb11160cf386fa5c9583647a66f22ffd76c7
Vincent Joy <vincentjoyhere@gmail.com>
2025-07-04 18:17:00 +0530

    updated Package file

--- Package.swift
+++ Package.swift
@@ -1,24 +1,34 @@
-// swift-tools-version: 6.1
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "LoggerSDK",
+    platforms: [
+        .iOS(.v12),
+        .macOS(.v10_14),
+        .tvOS(.v12),
+        .watchOS(.v5)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "LoggerSDK",
             targets: ["LoggerSDK"]),
     ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "LoggerSDK"),
+            name: "LoggerSDK",
+            dependencies: []),
         .testTarget(
             name: "LoggerSDKTests",
-            dependencies: ["LoggerSDK"]
-        ),
+            dependencies: ["LoggerSDK"]),
     ]
 )
