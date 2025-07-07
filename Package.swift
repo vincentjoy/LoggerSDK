211edf6bae155595b8df2c2727ee3e4e1e571434
Vincent Joy <vincentjoyhere@gmail.com>
2025-07-07 18:10:32 +0530

    Release commit

--- Package.swift
+++ Package.swift
@@ -17,10 +17,7 @@ let package = Package(
             name: "LoggerSDK",
             targets: ["LoggerSDK"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
